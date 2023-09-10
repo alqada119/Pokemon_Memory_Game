4e3bb1a341f48396393a068be482554122a93b17
Ahmed Alqadoucy <123011472+alqada119@users.noreply.github.com>
2023-09-09 20:33:59 -0500

    Final Version 1

--- App.js
+++ App.js
@@ -3,8 +3,13 @@ import './App.css';
 import { useState } from 'react';
 import Image from './assets/pokemon.png' //Have to import images 
 import { API } from './components/API';
+import { Score } from './components/score';
 
 function App() {
+  const [forceRerender, setForceRerender] = useState(0);
+  const rerenderAPI = () => {
+    setForceRerender(prevValue => prevValue + 1);
+  };
   return (
     <div className="Container">
       <div class="Header">
@@ -12,14 +17,15 @@ function App() {
           <img src={Image} height={200} width={300}/>
       </div>
       <div class="grid">
-          <div class="grid-item i1"><API/></div>
-          <div class="grid-item i2"><API/></div>
-          <div class="grid-item i3"><API/></div>
-          <div class="grid-item i4"><API/></div>
-          <div class="grid-item i5"><API/></div>
-          <div class="grid-item i6"><API/></div>
-          <div class="grid-item i7"><API/></div>
-          <div class="grid-item i8"><API/></div>
+          <div class="grid-item i1" onClick={rerenderAPI}><API key={forceRerender}/></div>
+          <div class="grid-item i2" onClick={rerenderAPI}><API key={forceRerender}/></div>
+          <div class="grid-item i3" onClick={rerenderAPI}><API key={forceRerender}/></div>
+          <div class="grid-item i4" onClick={rerenderAPI}><API key={forceRerender}/></div>
+          <div class="grid-item i5" onClick={rerenderAPI}><API key={forceRerender}/></div>
+          <div class="grid-item i6" onClick={rerenderAPI}><API key={forceRerender}/></div>
+          <div class="grid-item i7" onClick={rerenderAPI}><API key={forceRerender}/></div>
+          <div class="grid-item i8" onClick={rerenderAPI}><API key={forceRerender}/></div>
+          <Score/>
       </div>
     </div>
     
